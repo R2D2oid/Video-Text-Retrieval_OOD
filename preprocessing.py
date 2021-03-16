62f9f89c03a670f2f3c690faf291fe3a8a993f95
Zahra <zahra.vaseqi@mail.mcgill.ca>
2021-03-15 22:23:30 -0400

    normalized features, enabled sequence dim reduction.

--- preprocessing.py
+++ preprocessing.py
@@ -1,27 +1,26 @@
 import torch 
 import numpy as np
 import utilities as utils
-from dim_reduction import pca_dim_reduction
+from dim_reduction import pca_dim_reduction_2d
 
 def preprocess_embeddings(embeddings, num_feats, T):
     target_len = T * num_feats
-    processed_embeddings = []
-    count = 0
     dims = embeddings[0].shape
-    if num_feats < dims[0]:
-        print(f'reducing dims from {dims[0]} to {num_feats}')
-        embeddings = pca_dim_reduction(embeddings, pca_dims = num_feats)
-        
-    for emb in embeddings:
-        emb = emb.reshape(-1)
-        processed_emb = unify_embedding_length(emb, target_len)
-        processed_emb = processed_emb.reshape(-1, num_feats)
-        processed_embeddings.append(processed_emb)
-        count = count + 1
-        
-    return processed_embeddings
+    n_feats_orig = dims[0] if len(dims)==1 else dims[1]
+    
+    embeddings = [e.reshape(-1) for e in embeddings]
+    embeddings = [unify_embedding_length(e, target_len) for e in embeddings]
+    embeddings = [e.reshape(-1, num_feats) for e in embeddings]
+    
+    if num_feats < n_feats_orig:
+        embeddings = [e.cpu() for e in embeddings]
+        embeddings = pca_dim_reduction_2d(embeddings, pca_dims = num_feats)
+    
+    embeddings = [e/np.linalg.norm(e) for e in embeddings]
+    
+    return embeddings
 
-# unify feat size to ensure all embeddings are 1024xT
+# unify feat size to ensure all embeddings are n_feats x T
 # if embedding is smaller augment it with zeros at the end
 # if embedding is larger crop the extra rows
 def unify_embedding_length(emb, target_len):
@@ -30,12 +29,10 @@ def unify_embedding_length(emb, target_len):
         len_diff = target_len - emb_len
         zero_padding = np.zeros([len_diff])
         return torch.tensor(np.hstack((emb, zero_padding)))
-    elif emb_len > target_len:
-        return emb[0:target_len]
     else:
-        return emb
+        return torch.tensor(emb[0:target_len])
     
-def load_video_text_features(v_feats_dir, t_feats_path, n_feats_t, n_feats_v, T, L, split_path):
+def load_video_text_features(v_feats_dir, t_feats_path, n_feats_t, n_feats_v, T, L, split_path, n_max=None):
     # load pre-computed video features and text features
     vids = utils.load_video_feats(v_feats_dir) 
     caps = utils.load_picklefile(t_feats_path)
@@ -45,11 +42,16 @@ def load_video_text_features(v_feats_dir, t_feats_path, n_feats_t, n_feats_v, T,
         
     # keep only the data from the provided split
     vids, caps = load_split_data(vids, caps, split_path)
-
-    # preprocess video and text embeddings into proper format 
-    vids= preprocess_embeddings(vids, n_feats_v, T)
-    caps = preprocess_embeddings(caps, n_feats_t, L)
     
+    n_max = len(vids) if n_max is None else n_max
+
+    vids = vids[:n_max]
+    caps = caps[:n_max]
+    
+    # preprocess video and text embeddings into proper format 
+    vids = preprocess_embeddings(vids, n_feats_v, T)  
+    caps = preprocess_embeddings(caps, n_feats_t, L)
+  
     return vids, caps
 
 def load_split_data(vids, caps, split_path):
