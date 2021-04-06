0ce48eff260d5267750197902cb0ae73658c2d1e
Zahra <zahra.vaseqi@mail.mcgill.ca>
2021-04-06 16:19:01 -0400

    fixed sentence feat unify issue

--- data_provider.py
+++ data_provider.py
@@ -11,8 +11,11 @@ class TempuckeyVideoSentencePairsDataset(Dataset):
     def __init__(self, vid_feats_dir, txt_feats_path, split_ids, video_feat_seq_len, sent_feat_seq_len, transform=None):
         '''
         Args:
-            vid_feats_dir (string): Path to the video features directory
-            txt_feats_path (string): Path to sentence features pickle file
+            vid_feats_dir (string): str path to the video features directory
+            txt_feats_path (string): str path to sentence features pickle file
+            split_ids: list of video/sentence names in the split (ex. train/test/valid)
+            video_feat_seq_len: int length of the video feature vector sequence
+            sent_feat_seq_len: int length of the sentence feature vector sequence
             transform (callable, optional): Optional transform to be applied on a sample
         '''        
         # load pre-computed video features and text features
@@ -33,22 +36,30 @@ class TempuckeyVideoSentencePairsDataset(Dataset):
         return len(self.split_ids)
 
     def __getitem__(self, idx):
-        # idx is an integer index
-        # idx_name is the name of the video/sentence 
-        # example: 312_TRIPPING_2017-11-16-phi-wpg-home_00_17_33.052000_to_00_17_39.125000.mp4_00:17:46.532000_00:17:52.171000
-        idx_name = self.split_ids[idx]
+        '''
+        Input:
+            idx: integer index
+        Output:
+            sample: dict containing a video feature vector and a sentence feature vector
+        '''
+        # idx_name is the name of the video/sentence
+        # ex. 312_TRIPPING_2017-11-16-phi-wpg-home_00_17_33.052000_to_00_17_39.125000.mp4_00:17:46.532000_00:17:52.171000
+        idx_name = self.split_ids[idx] 
         
-        vid_feat = self.videos[idx_name][:self.video_feat_seq_len]
-        snt_feat = self.sents[idx_name].reshape(1,-1)[:self.sent_feat_seq_len]
+        vid_feat = self.videos[idx_name]
+        snt_feat = self.sents[idx_name].reshape(1,-1)
         
-#         vid_feat = unify_embedding_length(vid_feat, self.video_feat_seq_len)
-#         snt_feat = unify_embedding_length(snt_feat, self.sent_feat_seq_len)    
+        vid_feat = unify_embedding_length(vid_feat, self.video_feat_seq_len)
+        snt_feat = unify_embedding_length(snt_feat, self.sent_feat_seq_len)  
         
         sample = {'video': vid_feat, 'sent': snt_feat}
         return sample
 
     def get_dataset_mean_std(self):
-        # compute mean and std for video feature vectors
+        '''
+        Computes mean and standard deviation for video and sentence features, separately.
+        '''
+        # videos
         feat_vecs = []
         for k,v in self.videos.items():
             for vec in v:
@@ -57,7 +68,7 @@ class TempuckeyVideoSentencePairsDataset(Dataset):
         v_mean = feat_vecs.mean(axis=0)
         v_std = feat_vecs.std(axis=0)
 
-        # compute mean and std for text feature vectors
+        # sentences
         feat_vecs = []
         for k,v in self.sents.items():
             feat_vecs.append(v)
@@ -68,21 +79,25 @@ class TempuckeyVideoSentencePairsDataset(Dataset):
         return {'videos': (v_mean, v_std), 'sents': (t_mean, t_std)}
 
         
-# unify feat size to ensure all embeddings are n_feats x T
-# if embedding is smaller augment it with zeros at the end
-# if embedding is larger crop the extra rows
 def unify_embedding_length(emb, target_len):
-    emb_len = len(emb)
-    print('emb: ',emb_len)
-    print('target: ', target_len)
+    '''
+    Unify feat size to ensure all embeddings are n_feats x T
+        if embedding is smaller, then augment it with zeros at the end
+        if embedding is larger, crop the extra rows
+    '''
+    emb_len, num_feats = emb.shape
     if emb_len < target_len:
         len_diff = target_len - emb_len
-        zero_padding = np.zeros([len_diff])
-        return torch.tensor(np.hstack((emb, zero_padding)))
+        zero_padding = np.zeros([len_diff, num_feats])
+        return torch.tensor(np.vstack((emb, zero_padding))).float()
     else:
-        return torch.tensor(emb[0:target_len])
+        return torch.tensor(emb[0:target_len]).float()
     
 def remove_invalid_video_sentence_features(vids, caps):
+    '''
+    Remove invalid feature vector entries, including the empty vectors
+    Verifies that each video has a corresponding caption 
+    '''
     # remove video-captions that have an empty c3d features
     empty_ids = [k for k,v in vids.items() if len(v)==0]
     for e in empty_ids:
