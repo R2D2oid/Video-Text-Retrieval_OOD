9149d386ed37bfa83eb888416d6b0553ee556e02
Zahra <zahra.vaseqi@mail.mcgill.ca>
2021-04-16 02:44:26 -0400

    cleaned up sent feat extraction

--- extract_sent_feats.py
+++ extract_sent_feats.py
@@ -1,37 +1,45 @@
 import utilities as utils
 
-def load_model(model_name = 'universal-sentence-encoder'):
-	if model_name == 'universal-sentence-encoder':
-		import tensorflow as tf
-		import tensorflow_hub as hub
-		model = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
-	elif model_name == 'fasttext':
-		import sister
-		model = sister.MeanEmbedding(lang="en")
+def load_model(model_name = 'universal'):
+    if model_name == 'universal': # universal-sentence-encoder'
+        import tensorflow as tf
+        import tensorflow_hub as hub
+        model = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
+    elif model_name == 'fasttext':
+        import sister
+        model = sister.MeanEmbedding(lang="en")
+    else:
+        raise ValueError(f'Unknown model name {model_name}!')
+    return model
 
-	return model
+# model_name = 'universal' 
+# universal does not work locally. 
+# ran it on colab at: https://colab.research.google.com/drive/18PcCZiNsP6P078YlqxWw5mSLKnkzGG_Y#scrollTo=cPMCaxrZwp7t
+# resulting feats stored at /usr/local/data02/zahra/datasets/Tempuckey/sentence_segments/feats/text/universal/sentence_feats.pkl
+model_name = 'fasttext'
 
-home_dir = '/home/pishu/Desktop/repos'
-path_output = f'{home_dir}/VTR_OOD/output/sentence_segments'
-path_input = f'{home_dir}/VTR_OOD/output/sentence_segments.pkl'
+home_dir = '/usr/local/data02/zahra/datasets/Tempuckey/sentence_segments'
+path_input = f'{home_dir}/sentences.pkl'
+path_output = f'{home_dir}/feats/text/{model_name}'
 
+encoder = load_model(model_name = model_name)
 sent_segs = utils.load_picklefile(path_input)
-encoder = load_model(model_name = 'fasttext')
 
 id_to_feat = {}
 for sent_id,v in sent_segs.items():
-	snt_beg_ts = v['sent_start_time']
-	snt_end_ts = v['sent_end_time']
-	vid_beg_sec = v['vid_start_time']
-	vid_end_sec = v['vid_end_time']
-	vid_id = v['video_id'] 
-	snt = v['sentence']
-
-	snt_emb = encoder(snt)
-
-	id_to_feat[sent_id] = snt_emb
-
-path_ = f'{home_dir}/VTR_OOD/output/sent_feats.pkl'
+    snt_beg_ts = v['sent_start_time']
+    snt_end_ts = v['sent_end_time']
+    vid_beg_sec = v['vid_start_time']
+    vid_end_sec = v['vid_end_time']
+    vid_id = v['video_id'] 
+    snt = v['sentence']
+    
+    if model_name == 'universal':
+        snt_emb = encoder([snt])
+    else:
+        snt_emb = encoder(snt)
+    id_to_feat[sent_id] = snt_emb
+    
+path_ = f'{path_output}/sent_feats.pkl'
 utils.dump_picklefile(id_to_feat, path_)
 print(f'generated {path_}')
-	
